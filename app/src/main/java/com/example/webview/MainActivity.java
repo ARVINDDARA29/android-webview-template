c096119129b76aebe0618229da53590752238d3b
Arvind Bishnoi <ravibishnoirb047466@gmail.com>
2025-11-19 21:14:58 +0530

    Refactor WebSettings and change load URL
    Refactor WebSettings variable and update URL placeholder.

--- app/src/main/java/com/example/webview/MainActivity.java
+++ app/src/main/java/com/example/webview/MainActivity.java
@@ -1,13 +1,12 @@
 package com.example.webview;
 
-import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
-
     private WebView webView;
 
     @Override
@@ -17,19 +16,18 @@ public class MainActivity extends AppCompatActivity {
 
         webView = findViewById(R.id.webview);
 
-        WebSettings webSettings = webView.getSettings();
-        webSettings.setJavaScriptEnabled(true);
+        WebSettings ws = webView.getSettings();
+        ws.setJavaScriptEnabled(true);
 
         webView.setWebViewClient(new WebViewClient());
-        webView.loadUrl("https://google.com");  // Yaha apni Website ka URL daalo
+
+        // placeholder replaced by Actions:
+        webView.loadUrl("APP_URL_PLACEHOLDER");
     }
 
     @Override
     public void onBackPressed() {
-        if (webView.canGoBack()) {
-            webView.goBack();
-        } else {
-            super.onBackPressed();
-        }
+        if (webView.canGoBack()) webView.goBack();
+        else super.onBackPressed();
     }
 }
