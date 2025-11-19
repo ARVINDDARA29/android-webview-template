9f572af31e833c788e46d57e3708c0f8c0cf8beb
Arvind Bishnoi <ravibishnoirb047466@gmail.com>
2025-11-19 17:23:46 +0530

    Refactor WebView initialization in MainActivity

--- app/src/main/java/com/example/webview/MainActivity.java
+++ app/src/main/java/com/example/webview/MainActivity.java
@@ -3,17 +3,22 @@ package com.example.webview;
 import android.os.Bundle;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
-    private WebView webView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        webView = findViewById(R.id.webview);
-        WebSettings ws = webView.getSettings();
-        ws.setJavaScriptEnabled(true);
-        webView.loadUrl(getString(R.string.start_url));
+
+        WebView web = findViewById(R.id.webview);
+
+        WebSettings settings = web.getSettings();
+        settings.setJavaScriptEnabled(true);
+
+        String url = getString(R.string.start_url);
+        web.loadUrl(url);
     }
 }
