8d1aa5dea88a5f5b9a524cca210bbc0cd248e7e8
Arvind Bishnoi <ravibishnoirb047466@gmail.com>
2025-11-19 21:27:12 +0530

    Refactor MainActivity to use new WebView instance
    Replaced the existing WebView setup with a new instance and simplified the layout.

--- app/src/main/java/com/example/webview/MainActivity.java
+++ app/src/main/java/com/example/webview/MainActivity.java
@@ -1,33 +1,15 @@
 package com.example.webview;
 
 import android.os.Bundle;
-import android.webkit.WebSettings;
 import android.webkit.WebView;
-import android.webkit.WebViewClient;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
-    private WebView webView;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        webView = findViewById(R.id.webview);
-
-        WebSettings ws = webView.getSettings();
-        ws.setJavaScriptEnabled(true);
-
-        webView.setWebViewClient(new WebViewClient());
-
-        // placeholder replaced by Actions:
+        WebView webView = new WebView(this);
         webView.loadUrl("APP_URL_PLACEHOLDER");
-    }
-
-    @Override
-    public void onBackPressed() {
-        if (webView.canGoBack()) webView.goBack();
-        else super.onBackPressed();
+        setContentView(webView);
     }
 }
